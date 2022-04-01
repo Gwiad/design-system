c034cd96f546ca13d33c1f662ffe3166fd5482c1
Gwiad <53998994+Gwiad@users.noreply.github.com>
2022-04-01 13:29:16 +0200

    sb global font

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import { GlobalStyle } from '../src/shared/global';
+
+/*
+ * Global decorator to apply the styles to all stories
+ * Read more about them at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
